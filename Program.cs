9a03a8d09c98a300d2fe117c64942de4f7c53e1e
fathasss <fatihhas2609@gmail.com>
2021-09-29 10:25:33 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHWebAPIClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:53375/");                                             //Client edilecek domain
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); //Veri çıktısı format tipi ->json
+                HttpResponseMessage response;                                                                 //Http Mesajı
+                response = client.GetAsync("api/StudentApi").Result;                                      //Web sayfasındaki kullanılacak Api
+                if (response.IsSuccessStatusCode)
+                {
+                    var students = response.Content.ReadAsAsync<IEnumerable<Student>>().Result;
+                    foreach (var ogrenci in students)
+                    {
+                        Console.WriteLine("ID: {0} - FirstName : {1} - LastName: {2} " +
+                            "- Email: {3} - Mobile : {4} - Address : {5}",
+                            ogrenci.Id, ogrenci.FirstName, ogrenci.LastName, ogrenci.Email, ogrenci.Mobile,ogrenci.Address);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+        class Student
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Mobile { get; set; }
+            public string Address { get; set; }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("FHWebAPIClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FHWebAPIClient")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("0fe8906f-e5f0-4a45-b8f1-799ca2e0e404")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
